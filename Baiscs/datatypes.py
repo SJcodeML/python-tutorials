7710bed87ddbe5f0dd68d06493b0abddd434ec5e
SJcodeML <sidrajabin56@gmail.com>
2025-05-22 17:52:17 +0500

    code added

---
+++ Baiscs/datatypes.py
@@ -0,0 +1,88 @@
+# VARIABLES IN PYTHON :
+
+
+# # data
+a= 4
+b= 6
+c=8
+d=42
+
+
+e = a + b + c + d
+
+print(e)
+
+# equation
+a=2+3
+
+
+# example
+a=7
+b=6
+g = a-b
+print(g)
+
+
+f=3
+l=25
+
+k = f*l
+
+print(k)
+
+keywords = ["if", "else", "for", "while", "def", "return", "import", "as", "break", "continue", "class", "except", "finally", "try", "with", "lambda", "nonlocal", "global", "assert", "yield"]
+
+
+j = 56
+aisha = 89
+mikaal= 85
+sidra = 45
+
+house = aisha + j + mikaal + sidra
+
+print(house)
+
+# Variables in Python are used to store data that can be used and manipulated throughout your program. You don't need to declare variables 
+# explicitly; just assign a value to a name, and Python creates the variable automatically.
+
+# ### Examples:
+
+
+
+
+
+# Assigning an integer      NUMBER
+mikaal_age = 8
+print (mikaal_age)
+
+# # Assigning a float          DECIMAL
+mikaal_age = 8.6
+print (mikaal_age)
+
+# # Assigning a string       LETTER ->name or sentence 
+aisha_favourite_food = "aisha likes to eat mango"
+print(aisha_favourite_food)
+
+# # Assigning a boolean       TRUE OR FALSE
+is_student = True
+
+
+
+
+
+# -------------------------
+
+
+
+# # Multiple assignments
+# x, y, z = 1, 2, 3
+
+# # Displaying variables
+# print(age, pi, name, is_student)
+# ```
+
+# ### Key points:
+# - Variable names are case-sensitive (`age` and `Age` are different).
+# - Variable names should start with a letter or underscore, followed by letters, numbers, or underscores.
+# - No need to declare data types explicitly; Python infers them.
+
